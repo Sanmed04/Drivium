d48c0c11c2829eabc63d2a888fea06ab87de4357
Santiago Medina <santiagomedina111204@gmail.com>
2024-11-15 15:37:13 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import './index.css'
+
+export default function App() {
+  return (
+    <>
+      <h1 className=''>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+      </p>
+    </>
+  )
+}
+
